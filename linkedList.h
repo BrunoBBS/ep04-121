d9467c986631ea5c1564ea04c4e8d0c27a953d34
Bruno B. Scholl <bruno.bbs3@hotmail.com>
2016-11-14 21:52:27 -0200

    Comenta linkedlist.h

--- linkedList.h
+++ linkedList.h
@@ -5,22 +5,21 @@
 
 #include "types.h"
 
-/*This function receives a pointer to a poiter to the first item of the
- * linked list, and a string to put into the list, and puts it at the
- * start of the list*/
+/*Essa funcao recebe um ponteiro para um ponteiro do comeco da lista ligada, 
+ * uma string e a insere na primeira posicao da lista ligada*/
 void insert_first(pointer *start, char *x);
 
-/*Receives a pointer to a pointer to the first item of a sorted linked list
- * and a string to insert in the list, and puts it in the appropriate position
- * to keep the list sorted.*/
+/*Essa funcao recebe um ponteiro para um ponteiro do comeco da lista ligada, 
+ * uma string e a insere na posicao apropriada para que a lista continue 
+ * ordenada*/
 void insert_sorted(pointer *start, char *x);
 
-/*This function receives a poiter to the first item of a linked list and a
- * string to find in the list and returns a pointer to the item if it
- * exists or NULL if not.*/
+/*Essa funcao recebe um ponteiro para o inicio da lista ligada e uma string
+ * e retorna um ponteiro para a posicao da string na lista ou nulo caso ela
+ * nao esteja na lista.*/
 pointer find(pointer start, char *x);
 
-/*Creates a new linked list and returns a poiter to its first item*/
+/*Cria uma nova lista e retorna um ponteiro para seu primeiro item*/
 pointer new_list();
 
 #endif
